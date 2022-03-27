f2cdcc4f5f682c0328444b536823df2388dabe82
Kota Mizushima <mizukota@gmail.com>
2022-03-28 01:08:12 +0900

    Create 'Hello, Annotation' project

---
+++ src/main/java/com/github/kmizu/papt_example/Main.java
@@ -0,0 +1,8 @@
+package com.github.kmizu.papt_example;
+
+public class Main {
+    @Hello
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/com/github/kmizu/papt_example/apt/HelloProcessor.java
@@ -0,0 +1,31 @@
+package com.github.kmizu.papt_example.apt;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.SourceVersion;
+import javax.tools.Diagnostic;
+
+import java.util.Set;
+
+@SupportedAnnotationTypes("com.github.kmizu.papt_example.Hello")
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
+public class HelloProcessor extends AbstractProcessor {
+    private int round = 0;
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+        // case round == 0 is executed surely
+        if(round == 0) {
+            round ++;
+            return true;
+        }
+        var messager = super.processingEnv.getMessager();
+        messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, "Hello, Annotation!");
+        System.out.println("FOO");
+        return true;
+    }
+}
